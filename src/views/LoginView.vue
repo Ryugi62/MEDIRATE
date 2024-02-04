5c18f8887e6a0b4504149d97d70fbaa6308ab0fd
Ryugi62 <xorjf1027@naver.com>
2024-02-04 15:53:20 +0900

    design : 로그인 페이지 디자인 구현

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,30 +1,29 @@
 <template>
   <div class="login-container">
-    <h1>Login</h1>
-    <form @submit.prevent="onSubmit">
-      <div class="form-group">
-        <label for="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          v-model="loginData.username"
-          required
-        />
+    <h1>로그인</h1>
+    <form @submit.prevent="submitForm">
+      <div class="form-group" v-for="field in formFields" :key="field.id">
+        <div class="input-wrapper">
+          <input
+            :type="field.id === 'password' ? passwordFieldType : field.type"
+            :id="field.id"
+            v-model="field.model"
+            required
+            :placeholder="field.label"
+          />
+          <i
+            v-if="field.icon"
+            :class="field.iconClass"
+            @click="togglePasswordVisibility"
+          ></i>
+        </div>
       </div>
-
       <div class="form-group">
-        <label for="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          v-model="loginData.password"
-          required
-        />
+        <input type="checkbox" id="remember" v-model="rememberUsername" />
+        <label for="remember">아이디를 기억할까요?</label>
       </div>
-
       <button type="submit">Login</button>
     </form>
-    <p v-if="loginError" class="error-message">{{ loginError }}</p>
   </div>
 </template>
 
@@ -33,39 +32,80 @@ export default {
   name: "LoginView",
   data() {
     return {
-      loginData: {
-        username: "",
-        password: "",
-      },
-      loginError: "", // 로그인 오류 메시지
+      formFields: [
+        {
+          id: "username",
+          label: "아이디",
+          type: "text",
+          model: "",
+          icon: false,
+        },
+        {
+          id: "password",
+          label: "비밀번호",
+          type: "password", // 이 필드는 'password' 타입으로 시작
+          model: "",
+          icon: true,
+          iconClass: "far fa-eye", // 초기 아이콘 클래스는 'far fa-eye'
+        },
+      ],
+      passwordFieldType: "password", // 비밀번호 필드 타입 초기값을 'password'로 설정
+      rememberMe: false,
+      loginError: "",
     };
   },
 
+  mounted() {
+    this.loadUsername();
+  },
   methods: {
-    onSubmit() {
-      // 여기에서 로그인 처리를 시뮬레이션합니다.
-      const { username, password } = this.loginData;
+    submitForm() {
+      const { username, password } = this.formFields.reduce(
+        (acc, field) => ({ ...acc, [field.id]: field.model }),
+        {}
+      );
 
-      // 관리자 계정인지 확인합니다.
+      if (this.rememberMe) {
+        localStorage.setItem("rememberedUsername", username);
+      } else {
+        localStorage.removeItem("rememberedUsername");
+      }
+
       const isAdministrator = username === "admin" && password === "password";
-
-      // 간단한 로그인 시뮬레이션 (실제 서버 요청 대신 사용)
-      if (
-        (username === "user" && password === "password") ||
-        (username === "admin" && password === "password")
-      ) {
-        // action 로그인
+      if (this.isValidLogin(username, password)) {
         this.$store.dispatch("loginUser", {
           username,
           password,
           isAdministrator,
         });
-
-        // 로그인 후 홈 페이지로 이동
         this.$router.push({ name: "home" });
       } else {
-        // 로그인 실패 시 오류 메시지 표시
         this.loginError = "Invalid username or password. Please try again.";
+      }
+    },
+
+    isValidLogin(username, password) {
+      return (
+        (username === "user" && password === "password") ||
+        (username === "admin" && password === "password")
+      );
+    },
+    togglePasswordVisibility() {
+      this.passwordFieldType =
+        this.passwordFieldType === "password" ? "text" : "password";
+      this.formFields.find((field) => field.id === "password").iconClass =
+        this.passwordFieldType === "password"
+          ? "far fa-eye-slash"
+          : "far fa-eye"; // 아이콘 클래스 동적 변경
+    },
+    loadUsername() {
+      const rememberedUsername = localStorage.getItem("rememberedUsername");
+      if (rememberedUsername) {
+        const usernameField = this.formFields.find(
+          (field) => field.id === "username"
+        );
+        usernameField.model = rememberedUsername;
+        this.rememberMe = true;
       }
     },
   },
@@ -74,42 +114,89 @@ export default {
 
 <style scoped>
 .login-container {
-  max-width: 400px;
-  margin: 0 auto;
-  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+h1 {
+  margin-bottom: 48px;
 }
 
 .form-group {
-  margin-bottom: 15px;
+  display: flex;
+  margin-bottom: 16px;
 }
 
-.form-group label {
-  display: block;
-  margin-bottom: 5px;
-}
-
-.form-group input {
-  width: 100%;
-  padding: 10px;
-  border: 1px solid #ccc;
+.input-wrapper {
+  display: flex;
+  align-items: center;
+  padding: 8px;
+  border: 2px solid var(--light-gray);
   border-radius: 4px;
+  width: 358px;
+  height: 35px;
+  transition: border-color 0.3s; /* 부드러운 색상 전환을 위한 트랜지션 추가 */
+}
+
+.input-wrapper:focus-within {
+  border: 2px solid var(--blue); /* 여기에 추가 */
+}
+
+.input-wrapper input {
+  width: 100%;
+  padding: 8px 0;
+  font-size: 16px;
+  border: none;
+  outline: none;
+}
+
+.far {
+  padding: 8px;
+  color: var(--light-gray);
+}
+
+/* 커스텀 체크박스 스타일 */
+input[type="checkbox"] {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  position: relative;
+  width: 20px;
+  height: 20px;
+  border: 2px solid var(--light-gray);
+  border-radius: 4px;
+  outline: none;
+  background-color: transparent;
+}
+
+input[type="checkbox"]:checked {
+  background-color: var(--blue);
+  border-color: var(--blue);
+}
+
+input[type="checkbox"]:checked:after {
+  content: "";
+  position: absolute;
+  left: 5px;
+  top: 1px;
+  width: 3px;
+  height: 8px;
+  border: solid var(--white);
+  border-width: 0 3px 3px 0;
+  transform: rotate(45deg);
+}
+
+input[type="checkbox"]:checked {
+  background-color: var(--blue);
+  border: 2px solid var(--blue);
+  color: var(--white);
 }
 
 button {
-  padding: 10px 20px;
-  background-color: #2980b9;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-button:hover {
-  /* #2980b9보다 어두워져야됨 */
-  background-color: #1f6fb2;
-}
-
-button:active {
-  background-color: #1a5f9d;
+  width: 100%;
+  font-size: 16px;
+  height: 53px;
 }
 </style>
