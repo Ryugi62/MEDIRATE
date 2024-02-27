af80c07173212943091957c3748786036d08ae6e
Ryugi62 <xorjf1027@naver.com>
2024-02-27 13:59:43 +0900

    feat : 프록시 https 서버로 설정

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import axios from "axios";
 
 // Axios 인스턴스 생성 및 기본 URL 설정
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000", // 모든 요청에 자동으로 이 URL이 앞에 붙음
+  baseURL: "https://aialpa-eval.duckdns.org", // 모든 요청에 자동으로 이 URL이 앞에 붙음
 });
 
 // 애플리케이션 인스턴스를 생성
