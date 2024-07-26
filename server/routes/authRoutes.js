67a50feac308fc435c13e32870884b8687a5099e
Ryugi62 <xorjf1027@naer.com>
2024-07-26 11:42:42 +0900

    Refactor BBoxComponent to update square overlap calculations

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -30,7 +30,7 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign(
       { id: user.id, username: user.username.trim(), role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: "2h" }
+      { expiresIn: "20h" }
     );
 
     const userWithoutPassword = {
