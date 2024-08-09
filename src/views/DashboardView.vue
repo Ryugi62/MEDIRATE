697738d3de36be47fd9b19640588da7b9608eee2
Ryugi62 <xorjf1027@naer.com>
2024-08-09 19:45:36 +0900

    Update download filename to include total count of data

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -316,7 +316,10 @@ export default {
           const url = window.URL.createObjectURL(new Blob([response.data]));
           const link = document.createElement("a");
           link.href = url;
-          link.setAttribute("download", `Total_${this.data.length}.xlsx`);
+          link.setAttribute(
+            "download",
+            `Total_${this.data.length}responses.xlsx`
+          );
           document.body.appendChild(link);
           link.click();
           document.body.removeChild(link);
