b3db36ad91807d45d70aec35f7b788e9055490a8
Ryugi62 <xorjf1027@naer.com>
2024-08-09 17:59:39 +0900

    Update download filename to include total count of data

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -316,7 +316,7 @@ export default {
           const url = window.URL.createObjectURL(new Blob([response.data]));
           const link = document.createElement("a");
           link.href = url;
-          link.setAttribute("download", "searched_assignments.xlsx"); // 다운로드할 파일 이름 설정
+          link.setAttribute("download", `Total_${this.data.length}.xlsx`);
           document.body.appendChild(link);
           link.click();
           document.body.removeChild(link);
